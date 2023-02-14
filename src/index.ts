84ee46470dd738135046fc00b6e75fd9c1a455c4
Elad Hodeda <elad.hodeda2@gmail.com>
2023-02-14 20:11:09 +0200

    adding some functionality to the server

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,27 @@ import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import ITask from "./interfaces/ITask";
 import ITransition from "./interfaces/ITransition";
+import cors from "cors";
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 let tasks: ITask[] = [];
 let transitions: ITransition[] = [];
 
 app.use(bodyParser.json());
 
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
 app.get("/api/tasks", (req: Request, res: Response) => {
   res.json(tasks);
 });
-
 
 app.get("/api/transitions", (req: Request, res: Response) => {
   res.json(transitions);
